d30b0da10fc3e65aec261ce3ee1e3af3482064e3
Mario Junior <marioccomp@gmail.com>
2025-07-28 00:03:51 -0300

    uploading project

---
+++ src/components/Login.jsx
@@ -0,0 +1,46 @@
+import { useState } from 'react'
+import './Login.css'
+
+
+
+
+const Login = () => {
+
+    const [registerScreen, setRegisterScreen] = useState(false);
+
+    const toggleRegisterScreen = () => {
+        setRegisterScreen(!registerScreen);
+    }
+
+  return (
+    <div id='mainLogin'>
+        { !registerScreen && <div className="blockLogin">
+            <div className="header">
+                <h2>Faça seu Login</h2>
+            </div>
+            <form>
+                <input type="text"  placeholder='Digite seu nome de usuario'/>
+                <input type="text" placeholder='Digite sua senha'/>
+                <input type="submit" value='Entrar'/>
+            </form>
+                 <p>Não possui uma conta? <span onClick={toggleRegisterScreen}>Registre-se</span></p>
+
+        </div>}
+        
+        {registerScreen && <div className="blockLogin">
+            <div className="header">
+                <h2>Faça seu Registro</h2>
+            </div>
+            <form>
+                <input type="text" placeholder='Digite seu email'/>
+                <input type="text" placeholder='Digite seu usuario'/>
+                <input type="text" placeholder='Digite sua senha'/>
+                <input type="submit" value='Registrar'/>
+            </form>
+            <p>Já possui uma conta? <span onClick={toggleRegisterScreen}>Faça login</span></p>
+        </div>}
+    </div>
+  )
+}
+
+export default Login
